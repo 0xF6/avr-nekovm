7187005a08dc5fcda60a90e24300bfa057ba18ad
Yuuki Wesp <ls-micro@ya.ru>
2020-12-07 00:50:30 +0300

    drop files

--- extra/chocolatey/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-# Remove the dll files from C:\ProgramData\chocolatey\bin
-$chocoBin = Join-Path $env:ChocolateyInstall 'bin'
-$dllFiles = @('gcmt-dll.dll', 'neko.dll')
-foreach ($file in $dllFiles) {
-    $dllFile = Join-Path $chocoBin $file
-    Remove-Item "$dllFile"
-}
-
-Uninstall-ChocolateyEnvironmentVariable -VariableName NEKOPATH
